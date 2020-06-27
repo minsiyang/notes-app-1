944e6b7e65375b9417a0c3e119e5cfba0c99e6de
minsi yang <yangminsi@gmail.com>
2020-06-27 16:16:33 +0100

    trying out other code, failed

--- tests/note-controller-test.js
+++ tests/note-controller-test.js
@@ -1,6 +1,7 @@
 
 function noteControllerInstance() {
-   let controller = new NoteController();
+   let notelist = new NoteList();
+   let controller = new NoteController(notelist);
    expect.isTrue(controller instanceof NoteController)
 }
 
@@ -8,11 +9,10 @@ noteControllerInstance();
 
 function checkInnerHTMLPropertyValue() {
    let notelist = new NoteList();
-       notelist.addNote("Favourite food: pesto");
    let view = new NoteController(notelist); 
    view.getHTML(); 
-   let string = `<ul><a href="#0"><li><div> Favourite food: pest</div></li></a></ul>`
-   
+   console.log(document.getElementById('app').innerHTML)
+   let string = `<ul><a href="#0"><li><div>It is FRIDAY</div></li></a></ul>`
    expect.isTrue(document.getElementById('app').innerHTML === string)
 }
 checkInnerHTMLPropertyValue();
